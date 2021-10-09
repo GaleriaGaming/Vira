0d7e214a27edfb29f62c4092286f78a69cf18818
Galeria Gaming <juandination3.0@gmail.com>
2021-10-08 20:04:31 -0400

    make it better

---
+++ commands/various/reglas.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "reglas",
+  alias: ["rules", "editareglas"],
+
+  /**
+  * @param {Discord.Client} client
+  * @param {Discord.Message} message
+  * @param {string[]} args
+  **/
+
+async execute (client, message, args){
+
+    const rules = await client.channels.cache.get('891460235988979763').messages.fetch('896184979493646356')
+    
+    const newRules = args[0];
+    if(!newRules) {
+        return message.channel.send("❌ **• Debes decir las nuevas reglas (Para poner espacios pon \n)**")
+    } else {
+        const newRulesEmbed = new Discord.MessageEmbed()
+
+        .setTitle('° | REGLAS | °')
+        .setDescription(newRules)
+        
+        message.channel.send("✅ **• Las reglas han sido cambiadas**")
+
+        try {
+            rules.edit()
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+
+
+ }
+
+}
--- events/ready.js
+++ events/ready.js
@@ -27,7 +27,7 @@ const { name, version } = require('../config.json')
     presence();
   }, 7000)
 
-  await client.channels.cache.get('891460235988979763').messages.fetch('891460605335183411').then(m => console.log("Se ha cargado la verficacion"))
+  await client.channels.cache.get('891460235988979763').messages.fetch('896184979493646356').then(m => console.log("Se ha cargado la las reglas"))
   await client.channels.cache.get('891460235988979763').messages.fetch('891515298006573066').then(m => console.log("Se ha cargado la verficacion del zira"))
 
   console.log(`\n\nNombre: ${name}\nVersion: ${version}`)
